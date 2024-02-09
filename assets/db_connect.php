ae7cfca79744d68873ac5d2027637bb37bc25052
Usama Husain <nhusain34@gmail.com>
2024-02-09 19:25:24 +0530

    Property Page Dynamic ready

--- assets/db_connect.php
+++ assets/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, 'ars' );
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME );
 if(!$conn){
     die("Error :". mysqli_connect_error());
 }
